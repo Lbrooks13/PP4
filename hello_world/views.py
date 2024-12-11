8690a45c7f563e56571ec21123beaf0ced194d23
Liam Brooks <lbrooks1308@gmail.com>
2024-12-11 15:52:30 +0000

    folder move

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    context = {
-        'message': 'Hello, this is a message from the view!'
-    }
-    return render(request, 'index.html', context)
-    # return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World!")
